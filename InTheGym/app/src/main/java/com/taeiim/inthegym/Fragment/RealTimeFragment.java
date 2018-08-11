448410ea9c54521ab7445a1fcf829c5ac0edcf41
dogyhbin2 <dogyhbin12345@gmail.com>
2018-08-12 07:31:16 +0900

    dogyhbin2 finished 1 - dogyhbin2

--- InTheGym/app/src/main/java/com/taeiim/inthegym/Fragment/RealTimeFragment.java
+++ InTheGym/app/src/main/java/com/taeiim/inthegym/Fragment/RealTimeFragment.java
@@ -17,17 +17,31 @@ import com.taeiim.inthegym.R;
 public class RealTimeFragment extends Fragment {
 
     private View rootView;
-    ImageView imageView10;
+    ImageView imageView10, realtime_1, realtime_imageview1;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_realtime, container, false);
         imageView10 = rootView.findViewById(R.id.imageView10);
+        realtime_1 = rootView.findViewById(R.id.realtime_1);
+        realtime_imageview1 = rootView.findViewById(R.id.realtime_imageview1);
+
         imageView10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                realtime_imageview1.setVisibility(View.VISIBLE);
+                realtime_1.setVisibility(View.INVISIBLE);
+                imageView10.setVisibility(View.INVISIBLE);
+            }
+        });
 
+        realtime_imageview1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                realtime_imageview1.setVisibility(View.INVISIBLE);
+                realtime_1.setVisibility(View.VISIBLE);
+                imageView10.setVisibility(View.VISIBLE);
             }
         });
 
